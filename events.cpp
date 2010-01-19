516d60cdc3644492f34462bf01e38409d3b2881f
Alexey Noskov <alexey.noskov@gmail.com>
2010-01-19 14:16:11 +0300

    Support todos

--- events.cpp
+++ events.cpp
@@ -9,6 +9,7 @@
 #include <Akonadi/Item>
 
 #include <kcal/event.h>
+#include <kcal/todo.h>
 
 #include <boost/shared_ptr.hpp>
 
@@ -16,8 +17,8 @@
 K_EXPORT_PLASMA_RUNNER(events_runner, EventsRunner)
 
 // Mime types
-QString eventMimeType = "application/x-vnd.akonadi.calendar.event";
-QString todoMimeType = "application/x-vnd.akonadi.calendar.todo";
+QString eventMimeType( "application/x-vnd.akonadi.calendar.event" );
+QString todoMimeType( "application/x-vnd.akonadi.calendar.todo" );
 
 QString eventKeyword( "event" );
 QString todoKeyword( "todo" );
@@ -28,6 +29,14 @@ enum IncidentType {
     Event,
     Todo
 };
+
+static KDateTime variantToDateTime( const QVariant & var ) {
+    return var.type() == QVariant::Date ? KDateTime( var.toDate() ) : KDateTime( var.toDateTime() );
+}
+
+static QVariant dateTimeToVariant( const KDateTime & dt ) {
+    return dt.isDateOnly() ? QVariant( dt.date() ) : QVariant( dt.dateTime() );
+}
 
 EventsRunner::EventsRunner(QObject *parent, const QVariantList& args)
     : Plasma::AbstractRunner(parent, args)
@@ -82,13 +91,39 @@ void EventsRunner::match( Plasma::RunnerContext &context ) {
         QMap<QString,QVariant> data;
         data["type"] = Event;
         data["summary"] = summary;
-        data["start"] = startDate.toString();
+        data["dtStart"] = dateTimeToVariant( startDate );
+
+        Plasma::QueryMatch match( this );
+
+        match.setText( i18n( "Create event \"%1\" at %2", summary, startDate.toString( startDate.isDateOnly() ? "%d.%m.%Y" : "%H:%M %d.%m.%Y" ) ) );
+        match.setData( data );
+        match.setId( eventKeyword );
+        match.setRelevance( 0.8 );
+        match.setIcon( icon );
+
+        context.addMatch( term, match );
+     } else if ( term.startsWith( todoKeyword ) ) {
+        QStringList args = term.mid( eventKeyword.length() ).split( ";" );
+    
+        if ( args.size() < 2 || args[0].length() < 3 || args[1].length() < 3 )
+            return;
+        
+        QString summary = args[0].trimmed();
+        KDateTime dueDate = dateTimeParser.parse( args[1].trimmed() );
+        
+        if ( !dueDate.isValid() )
+            return;
+        
+        QMap<QString,QVariant> data;
+        data["type"] = Todo;
+        data["summary"] = summary;
+        data["dtDue"] = dateTimeToVariant( dueDate );
         
         Plasma::QueryMatch match( this );
         
-        match.setText( i18n( "Create event \"%1\" at %2 %3", summary, startDate.isDateOnly() ? "" : startDate.time().toString("hh:mm") , startDate.date().toString("dd.MM.yyyy") ) );        
+        match.setText( i18n( "Create todo \"%1\" due to %2", summary, dueDate.toString( dueDate.isDateOnly() ? "%d.%m.%Y" : "%H:%M %d.%m.%Y" ) ) );
         match.setData( data );
-        match.setId( "event" );  
+        match.setId( todoKeyword );  
         match.setRelevance( 0.8 );
         match.setIcon( icon );
         
@@ -109,12 +144,29 @@ void EventsRunner::run(const Plasma::RunnerContext &context, const Plasma::Query
         
         KCal::Event::Ptr event( new KCal::Event() );
         event->setSummary( data["summary"].toString() );
-        event->setDtStart( KDateTime::fromString( data["start"].toString() ) );
+        event->setDtStart( variantToDateTime( data["dtStart"] ) );
             
         Item item( eventMimeType );
         item.setPayload<KCal::Event::Ptr>( event );
             
         new Akonadi::ItemCreateJob( item, eventsCollection, this );
+    } else if ( data["type"].toInt() == Todo ) {
+        if ( !todoCollection.isValid() ) {
+            qDebug() << "No valid collection for todos available";
+            return;
+        }
+        
+        KCal::Todo::Ptr todo( new KCal::Todo() );
+        todo->setSummary( data["summary"].toString() );
+        todo->setDtDue( variantToDateTime( data["dtDue"] ) );
+        todo->setPercentComplete( 0 );
+        todo->setHasStartDate( false );
+        todo->setHasDueDate( true );
+            
+        Item item( todoMimeType );
+        item.setPayload<KCal::Todo::Ptr>( todo );
+            
+        new Akonadi::ItemCreateJob( item, todoCollection, this );
     }
 }
 
